c0e210f19bc6736667b5a0d48e412f6d9f2de6d9
Brian Wells <briswells@gmail.com>
2020-11-16 12:58:42 -0800

    Added unit tests fixed bug with sortDesc when numbers were the same

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,9 +14,99 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, is_simple_palindrome)
+TEST(PracticeTest, is_not_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Not a palindrome");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("racecar");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, symbols_not_palidrone)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("[;");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, empty_string_palidrone)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, single_char_palidrone)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("f");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, with_space_palidrone)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("and dna");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, long_palidrone)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("saippuakivikauppias");
+    ASSERT_TRUE(actual);
+}
+
+TEST(sortDescending, simple_desc)
+{
+    Practice obj;
+		int i(3), j(2), z(1);
+    bool actual = obj.sortDescending(i,j,z);
+    ASSERT_TRUE(actual);
+}
+
+TEST(sortDescending, simple_desc_all_same)
+{
+    Practice obj;
+		int i(1), j(1), z(1);
+    bool actual = obj.sortDescending(i,j,z);
+    ASSERT_TRUE(actual);
+}
+
+TEST(sortDescending, simple_desc_f2_same)
+{
+    Practice obj;
+		int i(1), j(1), z(2);
+    bool actual = obj.sortDescending(i,j,z);
+    ASSERT_TRUE(actual);
+}
+
+TEST(sortDescending, simple_desc_l2_same)
+{
+    Practice obj;
+		int i(2), j(1), z(1);
+    bool actual = obj.sortDescending(i,j,z);
+    ASSERT_TRUE(actual);
+}
+
+TEST(sortDescending, simple_desc_ends_same)
+{
+    Practice obj;
+		int i(1), j(2), z(1);
+    bool actual = obj.sortDescending(i,j,z);
+    ASSERT_TRUE(actual);
+}
+
+TEST(sortDescending, bigger_then_int)
+{
+    Practice obj;
+		int i(1000000000000000), j(2), z(1);
+    bool actual = obj.sortDescending(i,j,z);
     ASSERT_TRUE(actual);
 }
